3fe21e1b1299227d30895e52e11f42b16fcc6630
Ritesh-Patil18 <riteshdesale18@gmail.com>
2026-04-28 03:45:20 +0530

    Fix #7 Reverse Integer - add code

--- Medium/7_Reverse_Integer.java
+++ Medium/7_Reverse_Integer.java
@@ -14,3 +14,20 @@ class Solution {
     return (int ) l;       
     }
 }
+cat > Medium/7_Reverse_Integer.java << 'EOF'
+class Solution {
+    public int reverse(int x) {
+        long rev = x , l=0;
+        while(rev!=0)
+        {
+            l*=10;
+            l+=rev%10;
+            rev/=10;
+        }
+        if(l>Integer.MAX_VALUE || l<Integer.MIN_VALUE) 
+        {
+            return 0;
+        }
+    return (int ) l;       
+    }
+}
