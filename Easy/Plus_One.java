09ad2610eaba0c71d2cbe60aca019b7b600d211e
Ritesh-Patil18 <riteshdesale18@gmail.com>
2026-05-05 23:34:26 +0530

    Add: Pluse One - Easy

---
+++ Easy/Plus_One.java
@@ -0,0 +1,20 @@
+// Problem: Plus One
+// Difficulty: Easy
+// Runtime: 0ms | Beats: 100%
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        for(int i=digits.length-1; i>=0; i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;   
+        }
+        int[] result = new int[digits.length + 1];
+            result[0] = 1;
+            return result;
+    }
+}
